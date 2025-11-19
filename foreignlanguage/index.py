df6b37dbdf8f26f33579b567c0781e85d1ba6eac
minh khoi <minkhoi.dev@gmail.com>
2025-11-19 13:36:07 +0700

    sửa bài

---
+++ foreignlanguage/index.py
@@ -0,0 +1,14 @@
+from flask import render_template, request
+from foreignlanguage import app
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    with app.app_context():
+        app.run(debug=True)
