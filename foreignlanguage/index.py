2e167d5bdbf76524f0a4ffdbf289cf29f596f21a
minh khoi <minkhoi.dev@gmail.com>
2025-11-19 15:08:51 +0700

    change

--- foreignlanguage/index.py
+++ foreignlanguage/index.py
@@ -9,6 +9,10 @@ def index():
 def login():
     return render_template("login.html")
 
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
 if __name__ == "__main__":
     with app.app_context():
         app.run(debug=True)
