ded65da2bd98ded1da363e09db4c0281c8378fb6
HoPhamNhuQuynh <quynhho2611@gmail.com>
2025-12-04 11:05:56 +0700

    signin signup models

---
+++ foreignlanguage/static/js/scripts.js
@@ -0,0 +1,14 @@
+    function togglePass(inputId, icon) {
+        const input = document.getElementById(inputId);
+        const type = input.getAttribute('type') === 'password' ? 'text' : 'password';
+        input.setAttribute('type', type);
+
+        // Đổi icon
+        if (type === 'text') {
+            icon.classList.remove('fa-eye-slash');
+            icon.classList.add('fa-eye');
+        } else {
+            icon.classList.remove('fa-eye');
+            icon.classList.add('fa-eye-slash');
+        }
+    }
